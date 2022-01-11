f9a572de4c72943fa3a4a929dbf1e55260297e59
chronus-zerp <ryo.zspeed493@gmail.com>
2022-01-11 22:32:37 +0900

    1/11

---
+++ lib/action/hive/get_user_id.dart
@@ -0,0 +1,5 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+void getUserId(WidgetRef ref) {
+  var box = Hive.box('userId');
+}
---
+++ lib/action/hive/put_user_id.dart
@@ -0,0 +1,5 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+void putUserId(WidgetRef ref) {
+  var box = Hive.box('userId');
+}
